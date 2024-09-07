1c8dfd049e9f5fcb5fcc3aa0f3cdf974a6d41930
adeshazer123 <adeshazer@wisc.edu>
2024-09-06 23:55:46 -0500

    Add 2100.

--- src/pymodaq_plugins_keithley/hardware/keithley2100/keithley2100_VISADriver.py
+++ src/pymodaq_plugins_keithley/hardware/keithley2100/keithley2100_VISADriver.py
@@ -167,16 +167,16 @@ class Keithley2100VISADriver:
                     self._instr.write(mode + ':NPLC ' + str(nplc))
                 
                 if "TEMP" in mode:
-                    transducer = config["Keithley", "27XX", self.instr, module, 'CHANNELS', key, "transducer"].upper()
+                    transducer = config["Keithley", "2100", self.instr, module, 'CHANNELS', key, "transducer"].upper()
                     if "TC" in transducer:
-                        tc_type = config["Keithley", "27XX", self.instr, module, 'CHANNELS', key, "type"].upper()
-                        ref_junc = config["Keithley", "27XX", self.instr, module, 'CHANNELS', key, "ref_junc"].upper()
+                        tc_type = config["Keithley", "2100", self.instr, module, 'CHANNELS', key, "type"].upper()
+                        ref_junc = config["Keithley", "2100", self.instr, module, 'CHANNELS', key, "ref_junc"].upper()
                         self.mode_temp_tc(channel, transducer, tc_type, ref_junc)
                     elif "THER" in transducer:
-                        ther_type = config["Keithley", "27XX", self.instr, module, 'CHANNELS', key, "type"].upper()
+                        ther_type = config["Keithley", "2100", self.instr, module, 'CHANNELS', key, "type"].upper()
                         self.mode_temp_ther(channel, transducer, ther_type)
                     elif "FRTD" in transducer:
-                        frtd_type = config["Keithley", "27XX", self.instr, module, 'CHANNELS', key, "type"].upper()
+                        frtd_type = config["Keithley", "2100", self.instr, module, 'CHANNELS', key, "type"].upper()
                         self.mode_temp_frtd(channel, transducer, frtd_type)
 
 
